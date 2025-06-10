0468b2a15b495608e90b266f7d8cf9b2f81cfec2
HRITWIKA BANIK <101196425+HritwikaBanik@users.noreply.github.com>
2025-06-10 23:42:02 +0530

    Singleton pattern

---
+++ DESIGN PATTERNS/Singleton Pattern.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <thread>
+
+class Singleton {
+private:
+    Singleton() {
+        std::cout << "Singleton created\n";
+    }
+
+    // Prevent copy and assignment
+    Singleton(const Singleton&) = delete;
+    Singleton& operator=(const Singleton&) = delete;
+
+public:
+    static Singleton& getInstance() {
+        static Singleton instance; // Thread-safe in C++11+
+        return instance;
+    }
+
+    void doSomething() {
+        std::cout << "Using singleton instance\n";
+    }
+};
+
+
+void useSingleton() {
+    Singleton& s = Singleton::getInstance();
+    s.doSomething();
+}
+
+int main() {
+    std::thread t1(useSingleton);
+    std::thread t2(useSingleton);
+    std::thread t3(useSingleton);
+
+    t1.join();
+    t2.join();
+    t3.join();
+
+    return 0;
+}
+
+/*
+| Use Case              | Why Singleton?                             |
+| --------------------- | ------------------------------------------ |
+| Logger                | Only one logger needed across modules      |
+| Configuration Manager | Centralized configuration loaded once      |
+| Database Connection   | Only one DB handler to manage pool         |
+| Thread Pool           | One shared pool to manage worker threads   |
+| Cache Manager         | Shared cache used across system components |
+
+*/
